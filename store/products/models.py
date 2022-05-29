2d7e9e065c20b365666cfb4460d9175504140c6d
haw0kz <70606573+haw0kz@users.noreply.github.com>
2022-05-29 22:51:33 +0300

    last

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -22,3 +24,15 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.name} | {self.category.name}'
 
+
+class Basket(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
